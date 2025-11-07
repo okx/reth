13366983234474dca6394e6e902066253a828074
lucas.lim <lucas.lim@okg.com>
2025-11-07 17:21:08 +0800

    fix

--- crates/node/core/src/args/args_xlayer.rs
+++ crates/node/core/src/args/args_xlayer.rs
@@ -4,7 +4,6 @@ use clap::Args;
 /// X Layer Apollo configuration arguments
 #[derive(Clone, Debug, Default, Args)]
 #[group(id = "xlayer_apollo_args")]
-
 pub struct XLayerArgs {
     /// Enable Apollo
     #[command(flatten)]
