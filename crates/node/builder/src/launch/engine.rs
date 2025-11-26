fbb271569e7b5d1ca834e10a5a2ecd18eacff4f4
Niven <sieniven@gmail.com>
2025-11-26 16:36:46 +0800

    Add loggiing

--- crates/node/builder/src/launch/engine.rs
+++ crates/node/builder/src/launch/engine.rs
@@ -280,7 +280,7 @@ impl EngineNodeLauncher {
                 tokio::select! {
                     payload = built_payloads.select_next_some() => {
                         if let Some(executed_block) = payload.executed_block() {
-                            debug!(target: "reth::cli", block=?executed_block.recovered_block().num_hash(),  "inserting built payload");
+                            info!(target: "reth::cli", block=?executed_block.recovered_block().num_hash(),  "inserting locally built payload");
                             engine_service.orchestrator_mut().handler_mut().handler_mut().on_event(EngineApiRequest::InsertExecutedBlock(executed_block).into());
                         }
                     }
