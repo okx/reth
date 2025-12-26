3ba1e5d3820fff48ade7962769cb367ac183ba92
Niven <sieniven@gmail.com>
2025-12-26 18:54:37 +0800

    Add flashblocks payload to local executed block for prewarming (#79)
    * Add flashblocks payload to local executed block for prewarming
    
    * Fix to fully exhaustive, better rename
    
    * Fix test, should always calculate SR near expected end
    
    * Support async state root calc and sync commits
    
    * Fix
    
    * Fix bundle state on block building resolution
    
    * Better refactor, revert existing interface sigs
    
    * Add latest height check before sr calc
    
    * Add logging

--- crates/optimism/flashblocks/src/worker.rs
+++ crates/optimism/flashblocks/src/worker.rs
@@ -4,17 +4,15 @@ use alloy_primitives::B256;
 use op_alloy_rpc_types_engine::OpFlashblockPayloadBase;
 use reth_chain_state::ExecutedBlock;
 use reth_errors::RethError;
-use reth_evm::{
-    execute::{BlockBuilder, BlockBuilderOutcome},
-    ConfigureEvm,
-};
+use reth_evm::{execute::BlockBuilder, ConfigureEvm};
 use reth_execution_types::ExecutionOutcome;
 use reth_primitives_traits::{
-    AlloyBlockHeader, BlockTy, HeaderTy, NodePrimitives, ReceiptTy, Recovered,
+    header::HeaderMut, AlloyBlockHeader, Block, BlockTy, HeaderTy, NodePrimitives, ReceiptTy,
+    Recovered, RecoveredBlock,
 };
 use reth_revm::{cached::CachedReads, database::StateProviderDatabase, db::State};
 use reth_rpc_eth_types::{EthApiError, PendingBlock};
-use reth_storage_api::{noop::NoopProvider, BlockReaderIdExt, StateProviderFactory};
+use reth_storage_api::{BlockReaderIdExt, StateProviderFactory};
 use std::{
     sync::Arc,
     time::{Duration, Instant},
@@ -103,28 +101,24 @@ where
             let _gas_used = builder.execute_transaction(tx)?;
         }
 
-        // if the real state root should be computed
-        let BlockBuilderOutcome { execution_result, block, hashed_state, .. } =
-            if args.compute_state_root {
-                trace!(target: "flashblocks", "Computing block state root");
-                builder.finish(&state_provider)?
-            } else {
-                builder.finish(NoopProvider::default())?
-            };
+        // Build the block without computing state root
+        trace!(target: "flashblocks", "Building block without state root computation");
+        let (execution, bundle_state) = builder.finish_no_state_root(&state_provider)?;
+        let hashed_state = state_provider.hashed_post_state(&bundle_state);
 
         let execution_outcome = ExecutionOutcome::new(
-            state.take_bundle(),
-            vec![execution_result.receipts],
-            block.number(),
-            vec![execution_result.requests],
+            bundle_state,
+            vec![execution.execution_result.receipts],
+            execution.block.number(),
+            vec![execution.execution_result.requests],
         );
 
         let pending_block = PendingBlock::with_executed_block(
             Instant::now() + Duration::from_secs(1),
             ExecutedBlock {
-                recovered_block: block.into(),
+                recovered_block: execution.block.into(),
                 execution_output: Arc::new(execution_outcome),
-                hashed_state: Arc::new(hashed_state),
+                hashed_state: Arc::new(hashed_state.clone()),
                 trie_updates: Arc::default(),
             },
         );
@@ -132,10 +126,52 @@ where
             pending_block,
             args.last_flashblock_index,
             args.last_flashblock_hash,
+            hashed_state,
             args.compute_state_root,
         );
 
         Ok(Some((pending_flashblock, request_cache)))
+    }
+
+    pub(crate) fn compute_state_root(
+        &self,
+        computed_block: PendingFlashBlock<N>,
+    ) -> eyre::Result<Option<ExecutedBlock<N>>>
+    where
+        N::BlockHeader: HeaderMut,
+    {
+        let latest = self
+            .provider
+            .latest_header()?
+            .ok_or(EthApiError::HeaderNotFound(BlockNumberOrTag::Latest.into()))?;
+        let latest_hash = latest.hash();
+
+        if computed_block.parent_hash() != latest_hash {
+            trace!(target: "flashblocks", flashblock_parent = ?computed_block.parent_hash(), local_latest=?latest.num_hash(),"Skipping non consecutive flashblock");
+            // doesn't attach to the latest block
+            return Ok(None)
+        }
+
+        let state_provider = self.provider.history_by_block_hash(latest.hash())?;
+        let (state_root, trie_updates) =
+            state_provider.state_root_with_updates(computed_block.hashed_state.clone())?;
+
+        // Reconstruct the block with the newly computed state root
+        let original_block = computed_block.pending.block();
+        let senders = original_block.senders().to_vec();
+        let (mut header, body) = original_block.clone_block().split();
+        header.set_state_root(state_root);
+
+        // Re-populate the executed block
+        Ok(Some(ExecutedBlock {
+            recovered_block: Arc::new(RecoveredBlock::new_unhashed(
+                N::Block::new(header, body),
+                senders,
+            )),
+            execution_output: computed_block.pending.executed_block.execution_output.clone(),
+            hashed_state: Arc::new(computed_block.hashed_state),
+            trie_updates: Arc::new(trie_updates),
+        }))
     }
 }
 
